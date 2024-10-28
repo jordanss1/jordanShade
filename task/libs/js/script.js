fa00e6fb8621dd0aa804baa7720098e8318aa8a0
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2024-10-28 15:09:06 +0000

    finished php backend

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -12,6 +12,48 @@ $("#submit_1").on("click", () => {
       },
       method: "POST",
 
+      success: (result) => {
+        console.log(result);
+
+        if (result.status.code === 200) {
+          
+        }
+      },
+      error: (obj, details) => console.log(obj, details),
+    });
+  }
+});
+
+$("#submit_2").on("click", () => {
+  const inputVal = $("#input_2").val();
+
+  if (inputVal) {
+    $.ajax({
+      url: "libs/php/geocodeBackend.php",
+      dataType: "json",
+      data: {
+        postalCode: inputVal,
+      },
+      method: "POST",
+
+      success: (result) => console.log(result),
+      error: (obj, details) => console.log(obj, details),
+    });
+  }
+});
+
+$("#submit_3").on("click", () => {
+  const inputVal = $("#input_3").val();
+
+  if (inputVal) {
+    $.ajax({
+      url: "libs/php/geocodeBackend.php",
+      dataType: "json",
+      data: {
+        wikipediaSearch: inputVal,
+      },
+      method: "POST",
+
       success: (result) => console.log(result),
       error: (obj, details) => console.log(obj, details),
     });
--- task/libs/php/geocodeBackend.php
+++ task/libs/php/geocodeBackend.php
@@ -2,11 +2,44 @@
     ini_set("display_errors", true);
     header('Content-Type: application/json');
 
-    if (isset($_POST["searchTerm"])) {
-        echo "<script>console.log(" . $_POST["searchTerm"] . ");</script>";
+    $ch = curl_init();
+    $url = 'https://secure.geonames.org/';
+
+    function postData($url, $property) 
+    {
+        global $ch;
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, $url);
+        
+        $server_response = json_decode(curl_exec($ch), true);
+
+        $output['status']['code'] = "200";
+        $output['status']['name'] = "ok";
+        $output['status']['description'] = "success";
+        $output['data'] = $server_response[$property];
+
+        echo json_encode($output);
     }
 
-    echo json_encode("hey");
+    if (isset($_POST["searchTerm"])) {
+        $url = $url . "searchJSON?q=" . urlencode($_POST["searchTerm"]) . "&maxRows=5&username=jordanss";
+        
+        postData($url, "geonames");
+    }
+
+    if (isset($_POST["postalCode"])) {
+        $url = $url . "findNearbyPostalCodesJSON?postalcode=" . urlencode($_POST["postalCode"]) . "&radius=30&country=US&username=jordanss";
+
+        postData($url, "postalCodes");
+    }
+
+    if (isset($_POST["wikipediaSearch"])) {
+        $url = $url . "wikipediaSearchJSON?q=" . urlencode($_POST["wikipediaSearch"]) . "&maxRows=5&username=jordanss";
+
+        postData($url, "geonames");
+    }
 
 
+    curl_close($ch);
 ?>
